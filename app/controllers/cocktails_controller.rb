27526b583ae25e1a553d5706ba8f644a4ada0245
Alexis Roger <a.roger37@gmail.com>
2020-11-12 16:30:18 +0100

    Added DosesController and all methods except create who need to be fixed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,34 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: %i(edit update show destroy)
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def params_cocktail
+    params.require(:cocktail).permit(:name, :ingredient, :dose)
+  end
 end
